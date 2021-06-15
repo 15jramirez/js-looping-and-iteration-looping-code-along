f36fdcb021b8317576b1213732827d217a3e82ff
jramirez <ramirez.jocelyn73@yahoo.com>
2021-06-14 17:17:00 -0700

    commit tree 3e1ea6788779d70a57067601949f5939cd4e3242

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+const finishedCards = []
+function writeCards(names, event){
+    for ( let i = 0; i < names.length; i++ ){
+        finishedCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return finishedCards
+}
+
+function countDown(number){
+    while (number >= 0){
+        console.log(number--)
+    }
+}
